3560d317a9611e7eb0a24aef66ea7d5bb7a3e0d1
Ayyappan-M-P <mpayyappan2004@gmail.com>
2024-12-26 19:23:03 +0530

    second

--- ipad/src/App.jsx
+++ ipad/src/App.jsx
@@ -1,23 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 
 import Home from './screen/index'; // Ensure the path is correct
 import './index.css';
 
-const routes = [
-  {
-    path: '/',
-    element: <Home />, // Correct JSX syntax here
-  },
-];
-
-const router = createBrowserRouter(routes); // Renamed for clarity
-
 const App = () => {
   return (
     <MantineProvider>
-      <RouterProvider router={router} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </MantineProvider>
   );
 };
